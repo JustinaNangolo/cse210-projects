0e41cfb23452d2da9e1cbcaf5cfbff1f16ce6ce3
yourusernameJustinaNangolo <jtnangoloh@gmail.com>
2026-03-14 20:21:07 +0100

    save part 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,27 +1,27 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
     {
+        static void Main(string[] args)
+        {
         // --- PART 1: Core Requirements 1-3 (Single Guess) ---
-    Console.Write("What is the magic number? ");
-    int magicNumber = int.Parse(Console.ReadLine());
-    Console.Write("What is your guess? ");
-    int guess = int.Parse(Console.ReadLine());
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
 
-    if (guess < magicNumber)
-    {
-         Console.WriteLine("Higher");
-    }
-    else if (guess > magicNumber)
-    {
-        Console.WriteLine("Lower");
-    }
-    else
-    {
-        Console.WriteLine("You guessed it!");
-    }
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
 
@@ -29,29 +29,31 @@ class Program
 
 // --- PART 2: Adding the Loop ---
 /*
-Console.Write("What is the magic number? ");
-int magicNumber = int.Parse(Console.ReadLine());
+    Console.Write("What is the magic number? ");
+    int magicNumber = int.Parse(Console.ReadLine());
 
-int guess = -1; // Initialize with a value that won't match
+    int guess = -1; // Initialize with a value that won't match
 
-while (guess != magicNumber)
-{
-    Console.Write("What is your guess? ");
-    guess = int.Parse(Console.ReadLine());
+    while (guess != magicNumber)
+    {
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
 
-    if (guess < magicNumber)
-    {
-        Console.WriteLine("Higher");
-    }
-    else if (guess > magicNumber)
-    {
-        Console.WriteLine("Lower");
-    }
-    else
-    {
-        Console.WriteLine("You guessed it!");
+        if (guess < magicNumber)
+        {   
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
+
 */
 
 /*
