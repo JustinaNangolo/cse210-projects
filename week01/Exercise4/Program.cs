1fa8738781f054594d4da3af38e69011afad495c
yourusernameJustinaNangolo <jtnangoloh@gmail.com>
2026-03-14 21:12:24 +0100

    Prep 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumber  = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
+        
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Use numbers.Count to see how many items are in the list
+        double average = ((double)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = -1; // Assuming positive numbers, or use numbers[0]
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                // If this number is greater than the current max, it becomes the new max
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
