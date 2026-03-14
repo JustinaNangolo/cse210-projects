5778940672838bf6647c8b6e05bb7c90fc3e5350
yourusernameJustinaNangolo <jtnangoloh@gmail.com>
2026-03-14 21:35:30 +0100

    Part 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+        string userName = PromptUsername();
+        int userNumber = PromptUserNumber();
+
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, userNumber, squareNumber);
+
+
+    }
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program.");
+    }
+    static void PromptUsername()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static void PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static void SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string userName, int userNumber, int squareNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your favorite number {userNumber} is {squareNumber}.");
     }
 }
