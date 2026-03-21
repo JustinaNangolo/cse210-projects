6b8417db93a8b56d45c4ce992b8dd33106981c9c
yourusernameJustinaNangolo <jtnangoloh@gmail.com>
2026-03-21 14:15:27 +0100

    1

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,20 @@
+public class Job
+{
+    private string _company;
+    private string _jobTitle;
+    private string _startYear;
+    private string _endYear;
+
+    public Job(string company, string jobTitle, string startYear, string endYear)
+    {
+        _company = company;
+        _jobTitle = jobTitle;
+        _startYear = startYear;
+        _endYear = endYear;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} at {_company} ({_startYear} - {_endYear})");
+    }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Tech Company";
+        job1._startYear = "2020";
+        job1._endYear = "2022";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Web Developer";
+        job2._company = "Web Solutions";
+        job2._startYear = "2018";
+        job2._endYear = "2020";
+
+        Resume resume = new Resume();
+        resume._name = "John Doe";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+        
+
     }
 }
