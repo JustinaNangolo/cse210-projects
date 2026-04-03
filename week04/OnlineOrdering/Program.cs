dc06dcb2e7698c61cd9568e7443babab8988cbb1
yourusernameJustinaNangolo <jtnangoloh@gmail.com>
2026-04-03 16:47:58 +0200

    1

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,19 +5,19 @@ class Program
     static void Main(string[] args)
     {
         // Order 1: USA Customer
-        Address address1 = new Address("123 Llama Lane", "Rexburg", "ID", "USA");
-        Customer customer1 = new Customer("Brother Burton", address1);
+        Address address1 = new Address("123 Hello Lane", "California", "CA", "USA");
+        Customer customer1 = new Customer("Brother Henry", address1);
         Order order1 = new Order(customer1);
-        order1.AddProduct(new Product("Mechanical Keyboard", "KBD-77", 85.00, 1));
-        order1.AddProduct(new Product("USB-C Hub", "USB-09", 15.50, 2));
+        order1.AddProduct(new Product("Mac book Keyboard", "MBK-77", 35.00, 1));
+        order1.AddProduct(new Product("Macbook Charger", "MacC-09", 29.50, 2));
 
         // Order 2: International Customer
-        Address address2 = new Address("456 Maple Rd", "Toronto", "ON", "Canada");
-        Customer customer2 = new Customer("Jane Doe", address2);
+        Address address2 = new Address("412 Maple Rd", "Toronto", "ON", "Canada");
+        Customer customer2 = new Customer("Justina Daniel", address2);
         Order order2 = new Order(customer2);
-        order2.AddProduct(new Product("Gaming Mouse", "MSE-01", 45.00, 1));
-        order2.AddProduct(new Product("Mouse Pad", "PAD-02", 12.00, 1));
-        order2.AddProduct(new Product("Monitor Stand", "STD-05", 29.99, 1));
+        order2.AddProduct(new Product("HDMI Cable", "HDMI-01", 15.00, 1));
+        order2.AddProduct(new Product("Phone Case", "PC-02", 12.00, 1));
+        order2.AddProduct(new Product("Iphone charger", "IC-05", 24.99, 1));
 
         // Display results
         DisplayOrderDetails(order1);
