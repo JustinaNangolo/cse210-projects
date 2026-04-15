d024255a46205a380d435e2240c7654f99992333
yourusernameJustinaNangolo <jtnangoloh@gmail.com>
2026-04-15 14:57:39 +0200

    1

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,22 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        // Create one of each activity
+        Running run = new Running("03 Nov 2022", 30, 3.0);
+        Cycling cycle = new Cycling("03 Nov 2022", 30, 12.0);
+        Swimming swim = new Swimming("03 Nov 2022", 30, 20);
+
+        activities.Add(run);
+        activities.Add(cycle);
+        activities.Add(swim);
+
+        // Iterate and display summaries
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
